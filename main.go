2f1eee32c42875ddb6711f8d5735c7211f4bf47a
WarriorIcon <66060955+WarriorIcon@users.noreply.github.com>
2023-03-08 11:35:09 -0500

    print

--- main.go
+++ main.go
@@ -5,5 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Print("hello")
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
+
+	fmt.Printf("Welcome to the %v booking application!\n", conferenceName) 
+	fmt.Printf("We have %v tickets and %v are still available!\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets to attend!")
 }
+
+
+
